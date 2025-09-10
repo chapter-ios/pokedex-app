31086e03d1711cad22055ff4882da22fa8b50c7c
chapter-ios <wicaksana.satriya@ymail.com>
2025-09-10 16:15:15 +0700

    first initial commit

---
+++ pokedex-app/pokedex_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_appApp.swift
+//  pokedex-app
+//
+//  Created by PhinCon on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
